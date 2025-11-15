908e7feb6c70fab3902fafc333c3e48d07d48beb
maksys <maksys2011@yandex.ru>
2025-11-15 13:25:23 +0300

    Добавил решение задачи 14

---
+++ Leedcod/array/14. Longest Common Prefix.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) 
+    {
+        if (strs.empty()) return " ";
+        std::string prefix = strs[0];
+        for (const auto& it : strs) {
+            int i = 0;
+            if (prefix[i] != it[i]) return "";
+            while (prefix[i] == it[i] && i < it.size()) {
+                i++;
+            }
+            prefix = prefix.substr(0,i);
+        }
+        return prefix;
+    }
+};
